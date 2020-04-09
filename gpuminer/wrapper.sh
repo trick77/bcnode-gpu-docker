52d735b4f76f47fb69f63c49ad5b4be1c6a15662
trick77 <jan@trick77.com>
2020-04-09 09:11:04 +0200

    Showing if container has access to GPU.

--- gpuminer/wrapper.sh
+++ gpuminer/wrapper.sh
@@ -3,6 +3,12 @@ set -e
 
 export LD_LIBRARY_PATH="/root/grpc/build:$LD_LIBRARY_PATH"
 
+echo "*** Let's see if we have access to the GPU from within the container:"
+nvcc --version
+nvidia-smi
 # Periodically terminate (Docker will restart it automatically) the
 # GPU miner to clear "some nasty work backup". You'd have to ask LG.
+echo "*** Starting the miner..."
 timeout -s INT 5m ./miner
+echo "*** Terminating GPU miner (this is NOT an error!)..."
+echo
