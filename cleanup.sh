76187e01c3a3cda821267cb75c90198b51668f8f
trick77 <jan@trick77.com>
2020-04-09 08:47:25 +0200

    Colorful output, yay.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-echo "*** Killing containers if running, cleaning up..."
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+echo -e "${GREEN}Killing containers if running, cleaning up...${NC}"
 docker rm -f bcnode
 docker rm -f gpuminer
 docker network rm waietng
