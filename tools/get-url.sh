ae248a641ece3068ffcdee656268d394468d6d3a
trick77 <jan@trick77.com>
2020-04-13 09:36:07 +0200

    Made it more readable.

--- tools/get-url.sh
+++ tools/get-url.sh
@@ -2,4 +2,17 @@
 set -eo pipefail
 
 . ../config
-curl -s --basic --user ":${BC_SCOOKIE}" -H "content-type: application/json" -H "accept: application/json" -d '{ "jsonrpc": "2.0", "id": 123, "method": "getSettings", "params": [] }' http://localhost:3000/rpc | jq  --raw-output '.result.ngrokTunnel'
+
+{
+curl -s --basic --user ":${BC_SCOOKIE}" http://localhost:3000/rpc \
+    -H "content-type: application/json" \
+    -H "accept: application/json" \
+    -d  @- <<EOF
+{
+  "jsonrpc": "2.0",
+  "id": 123,
+  "method": "getSettings",
+  "params": []
+}
+EOF
+} | jq  --raw-output '.result.ngrokTunnel'
