841a958a120ab9ccf1f087d9131b99e32308bc8d
trick77 <jan@trick77.com>
2020-04-18 07:37:27 +0200

    Cleaned up wrapper script.

--- gpuminer/wrapper.sh
+++ gpuminer/wrapper.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-#echo "*** Let's see if and which version of Nvidia CUDA is available in the container:"
-#nvcc --version
 echo "*** Check the following output if this container has access to one or more GPUs:"
 nvidia-smi
-# Periodically terminate (Docker will restart it automatically) the
-# GPU miner to clear "some nasty work backup". You'd have to ask LG.
 echo "*** Starting miner..."
 ./miner
-echo "*** Terminating miner (this is NOT an error!)..."
 echo
