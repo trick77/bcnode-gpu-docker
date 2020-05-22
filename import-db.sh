1eab50c5084477e292edd39cebb10af552e20f2a
trick77 <jan@trick77.com>
2020-05-22 13:17:01 +0200

    Added database import script.

--- import-db.sh
+++ import-db.sh
@@ -17,18 +17,20 @@ database_location=$1
 bcnode_container_name=bcnode
 database_volume_name=db
 
-echo -e "${GREEN}Importing bcnode database from ${database_location}...${NC}"
-
 if [ ! -f "${database_location}" ]; then
   echo -e "${RED}Error: Missing import file ${database_location}${NC}"
   exit 1
 fi
 
-existing_volume=$(docker volume ls -q -f name=${database_volume_name} | grep -w ${database_volume_name})
-if [ ${existing_volume} != "${database_volume_name}" ]; then
-  echo -e "${YELLOW}Warning: No named volume \"${database_volume_name}\" found, creating it..."
-  echo -e "Make sure it's going to be attached to the ${bcnode_container_name} container.${NC}"
-  docker volume create ${database_volume_name}
+echo -e "${GREEN}Preparing to import the blockchain database from ${database_location}...${NC}"
+docker rm -f importdb > /dev/null 2>&1 || true
+
+read -p "This will stop the container if ${bcnode_container_name} is currently running. Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e "${GREEN}This was a close call man!${NC}"
+    exit 1
 fi
 
 if [ -f "./cleanup.sh" ]; then
@@ -37,7 +39,22 @@ else
   echo -e "${GREEN}Stopping current ${bcnode_container_name} container if running...${NC}"
   docker rm -f ${bcnode_container_name} > /dev/null 2>&1 || true
 fi
-docker rm -f importdb > /dev/null 2>&1 || true
+
+existing_volume=$(docker volume ls -q -f name=${database_volume_name} | grep -w ${database_volume_name}) || true
+if [ -z ${existing_volume} ]; then
+  echo -e "${YELLOW}Warning: No named Docker volume \"${database_volume_name}\" found, creating it..."
+  echo -e "Make sure it's going to be attached to the ${bcnode_container_name} container!${NC}"
+  docker volume create ${database_volume_name}
+else
+  read -p "This will delete the local blockchain copy in Docker volume ${database_volume_name}. Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+      echo -e "${GREEN}This was a close call man!${NC}"
+      exit 1
+  fi
+  docker volume rm ${database_volume_name}
+fi
 
 echo -e "${GREEN}Starting dummy container to access ${database_volume_name} volume...${NC}"
 docker run -d --rm --name importdb -v ${database_volume_name}:/root alpine tail -f /dev/null
@@ -56,4 +73,3 @@ rm -rf ${tmp_dir}
 
 echo -e "${GREEN}Done."
 echo -e "You can start the ${bcnode_container_name} container now!${NC}"
-
