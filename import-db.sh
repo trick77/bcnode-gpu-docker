630fa6d0774d0b4922bfa82894bb23a31ab25615
trick77 <trick77@users.noreply.github.com>
2020-08-14 11:37:11 +0200

    Update import-db.sh

--- import-db.sh
+++ import-db.sh
@@ -9,7 +9,8 @@ YELLOW='\033[0;33m'
 if [ $# -eq 0 ]
   then
     echo -e "${RED}Error: No arguments supplied".
-    echo -e "Usage: $0 /path/to/bcnode-db-2029-11-12_13-14-15.tar.gz${NC}"
+    echo -e "Usage: $0 /path/to/bcnode-db-2029-11-12_13-14-15.tar.gz"
+    echo -e "       $0 /path/to/_easysync_db.zip${NC}"
     exit 1
 fi
 
@@ -61,11 +62,21 @@ docker run -d --rm --name importdb -v ${database_volume_name}:/root alpine tail 
 
 tmp_dir=`mktemp -d`
 echo -e "${GREEN}Extracting database $1 to ${tmp_dir}${NC}"
-tar -xf  $1 -C ${tmp_dir}
-rm ${tmp_dir}/db/IDENTITY > /dev/null 2>&1 || true
+if [ ${1: -3} == ".gz" ]; then
+  tar -xf $1 -C ${tmp_dir}
+else
+  if [ ${1: -4} == ".zip" ]; then
+    unzip $1 -d ${tmp_dir}
+  fi
+fi
+rm ${tmp_dir}/_data/db/IDENTITY > /dev/null 2>&1 || true
+rm ${tmp_dir}/_data/.chainstate.db > /dev/null 2>&1 || true
+
+echo -e "${GREEN}Latest database timestamp:"
+ls -Artls ${tmp_dir}/_data/db/*.sst | tail -n 1
 
 echo -e "${GREEN}Copying database to volume...${NC}"
-docker cp ${tmp_dir}/* importdb:/root
+docker cp ${tmp_dir}/_data/* importdb:/root
 
 echo -e "${GREEN}Cleaning up...${NC}"
 docker rm -f importdb > /dev/null 2>&1
